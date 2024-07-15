9f6377613857e90c5d83643107acccfda1acd1ae
mihretugonche <147416994+mihretugonche@users.noreply.github.com>
2024-07-15 20:39:19 +0300

    update index

--- mail.php
+++ mail.php
@@ -1,13 +1,10 @@
 <?php
-
-
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-// $phone= $_POST['phone'];
 $to = "mihretupg@mail.com";
 $subject = "Mail From Kindtex website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message ;
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@kindtex.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
